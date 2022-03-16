4bbf137c8ae8205f2cbc8fd069b1925c9c3d4b66
Harini Garre <harini.garre@gmail.com>
2022-03-16 21:23:46 +0530

    add CSP

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,26 +2,26 @@ const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
-// const CSP = 'Content-Security-Policy';
-// const POLICY =
-//   "default-src 'self' https://*.mapbox.com ;" +
-//   "base-uri 'self';block-all-mixed-content;" +
-//   "font-src 'self' https: data:;" +
-//   "frame-ancestors 'self';" +
-//   "img-src http://localhost:3000 'self' blob: data:;" +
-//   "object-src 'none';" +
-//   "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
-//   "connect-src 'self' ;" +
-//   "script-src-attr 'none';" +
-//   "style-src 'self' https: 'unsafe-inline';" +
-//   'upgrade-insecure-requests;';
+const CSP = 'Content-Security-Policy';
+const POLICY =
+  "default-src 'self' https://*.mapbox.com ;" +
+  "base-uri 'self';block-all-mixed-content;" +
+  "font-src 'self' https: data:;" +
+  "frame-ancestors 'self';" +
+  "img-src http://localhost:3000 'self' blob: data:;" +
+  "object-src 'none';" +
+  "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+  "connect-src 'self' https://*.herokuapp.com/login;" +
+  "script-src-attr 'none';" +
+  "style-src 'self' https: 'unsafe-inline';" +
+  'upgrade-insecure-requests;';
 
 const router = express.Router();
 
-// router.use((req, res, next) => {
-//   res.setHeader(CSP, POLICY);
-//   next();
-// });
+router.use((req, res, next) => {
+  res.setHeader(CSP, POLICY);
+  next();
+});
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
